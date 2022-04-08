2f094a7dfa6e2c6e9ad2707e11445514d2af2b5c
Ciwinforjsstuding <spid2001@yandex.ru>
2022-04-08 18:21:28 +0300

    build(ChatItem): add the selected chat item

--- src/components/Chat/ChatItem/index.tsx
+++ src/components/Chat/ChatItem/index.tsx
@@ -13,13 +13,20 @@ interface Props {
 export const ChatItem = ({ title }: Props) => {
     const { chooseChat, saveDiffInChat, resetDialog } = useAction();
     const { choosedChat } = useSelector((state: RootState) => state.dialog);
+
     const chatItemHandler = () => {
         saveDiffInChat(choosedChat!);
         chooseChat(title);
     };
 
+    const isSelectedChatItem =
+        title === choosedChat?.titleChat ? "chat-item_selected" : "";
+
     return (
-        <div onClick={chatItemHandler} className="chat-item">
+        <div
+            onClick={chatItemHandler}
+            className={`chat-item ${isSelectedChatItem}`}
+        >
             <span className="chat-item__title">{title}</span>
             <button onClick={resetDialog} className="chat-item__btn">
                 Очистить
