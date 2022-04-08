4c074347e6150e2e38c93c211c74318be6705387
Ciwinforjsstuding <spid2001@yandex.ru>
2022-04-08 18:15:21 +0300

    build(App): add the initial to LocalStorage

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useAction } from "../../hooks/useAction";
+import { RootState } from "../../store";
+import { getChatsFromLocalStorage } from "../../store/utils";
+import { Dialog } from "../Dialog";
+import { NavBar } from "../NavBar";
 
 import "./app.scss";
 
 export const App = () => {
-    return <h1 className="test-text">Hello</h1>;
+    const { initialLocaStorage } = useAction();
+    const { choosedChat } = useSelector((state: RootState) => state.dialog);
+    useEffect(() => {
+        const chats = getChatsFromLocalStorage();
+        initialLocaStorage(chats);
+        /*eslint-disable */
+    }, []);
+    return (
+        <div className="app">
+            <NavBar />
+            {choosedChat && <Dialog />}
+        </div>
+    );
 };
